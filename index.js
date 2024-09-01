42c33585864d072f4ad792420643756c61e04ae9
RabinaRokaa <rabinaroka112@gmail.com>
2024-09-01 12:18:24 +0545

    Basics JSX element

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const JSX = <h1>Hello JSX!</h1>;
+const JSXX = <h1>Hello JSX!</h1>;
 const JSXx = <div>
 <h1></h1>
 <p></p>
@@ -10,3 +10,54 @@ const JSXx = <div>
 </ul>
 
 </div>
+
+const JSXxx = (
+    {/*
+    <div>
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+    </div>
+    */}
+  );
+
+
+  const JSXs = (
+
+    <div>
+    
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+      {/* */} 
+    </div>
+    
+    
+  );
+
+
+  const JSX = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+  );
+  // Add your code below this line
+  
+  ReactDOM.render(JSX, document.getElementById('challenge-node'));
+
+
+  //Define a html class
+  const JSXh = (
+    <div className="myDiv">
+      <h1>Add a class to this div</h1>
+    </div>
+  );
+
+
+  //self closing JSX Tags
+  const JSXS = (
+    <div>
+      <h2>Welcome to React!</h2> <br />
+      <p>Be sure to close all tags!</p>
+      <hr />
+    </div>
+  );
--- readme.txt
+++ readme.txt
@@ -4,3 +4,13 @@
 ###React uses a syntax extension of JavaScript called JSX that allows you to write HTML directly within JavaScript
 
 ###However, because JSX is not valid JavaScript, JSX code must be compiled into JavaScript. The transpiler Babel is a popular tool for this process.
+
+##To put comments inside JSX, you use the syntax {/* */} to wrap around the comment text.
+
+##With React, we can render this JSX directly to the HTML DOM using React's rendering API known as ReactDOM.
+
+
+###One key difference in JSX is that you can no longer use the word class to define HTML classes. This is because class is a reserved word in JavaScript. Instead, JSX uses className
+
+
+###In JSX, the rules are a little different. Any JSX element can be written with a self-closing tag, and every element must be closed. The line-break tag, for example, must always be written as <br /> in order to be valid JSX that can be transpiled.
